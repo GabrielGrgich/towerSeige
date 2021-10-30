439642d47de79c6cc259be0f3e47f38e88472409
GabrielGrgich <72478000+GabrielGrgich@users.noreply.github.com>
2021-10-30 13:07:49 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,37 +5,62 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var  polygon, sling1, groundObject;
+var  polygon, slingShot, groundObject1,groundObject2;
 var box1,box2,box3,box4,box5,box6,box7,box8,box9,box10,box11,box12;
 
-function preload() {
-    
+function preload(){
+polygon_img=loadImage("polygon.png");
 }
 
 function setup(){
-    var canvas = createCanvas(1300,600);
+    var canvas = createCanvas(900,400);
     engine = Engine.create();
     world = engine.world;
 
 
-    box1 = new Box(1000,300,50,50);
-    box2 = new Box(1050,300,50,50);
-    box3 = new Box(1100,300,50,50);
-    box4 = new Box(950,300,50,50);
-    box5 = new Box(900,300,50,50);
-    box6 = new Box(925,250,50,50);
-    box7 = new Box(975,250,50,50);
-    box8 = new Box(1025,250,50,50);
-    box9 = new Box(1075,250,50,50);
-    box10 = new Box(950,200,50,50);
-    box11 = new Box(1000,200,50,50);
-    box12 = new Box(1050,200,50,50);
-    groundObject = new ground(1000,400,500,20);
+    //level one
+  block1 = new Box(300,275,30,40);
+  block2 = new Box(330,275,30,40);
+  block3 = new Box(360,275,30,40);
+  block4 = new Box(390,275,30,40);
+  block5 = new Box(420,275,30,40);
+  block6 = new Box(450,275,30,40);
+  block7 = new Box(480,275,30,40);
+  //level two
+  block8 = new Box(330,235,30,40);
+  block9 = new Box(360,235,30,40);
+  block10 = new Box(390,235,30,40);
+  block11 = new Box(420,235,30,40);
+  block12 = new Box(450,235,30,40);
+  //level three
+  block13 = new Box(360,195,30,40);
+  block14 = new Box(390,195,30,40);
+  block15 = new Box(420,195,30,40);
+  //top
+  block16 = new Box(390,155,30,40);
 
-    polygon = new Polygon(200,200);
+  //set 2 for second stand
+  //level one
+  blocks1 = new Box(640,175,30,40);
+  blocks2 = new Box(670,175,30,40);
+  blocks3 = new Box(700,175,30,40);
+  blocks4 = new Box(730,175,30,40);
+  blocks5 = new Box(760,175,30,40);
+  //level two
+  blocks6 = new Box(670,135,30,40);
+  blocks7 = new Box(700,135,30,40);
+  blocks8 = new Box(730,135,30,40);
+  //top
+  blocks9 = new Box(700,95,30,40);
 
+    groundObject1 = new ground(390,300,250,10);
+    groundObject2 = new ground(700,200,200,10);
+
+    polygon = Bodies.circle(50,200,20);
+    World.add(world,polygon);
+
+    slingShot = new SlingShot(this.polygon,{x:100,y:200});
     
-    //slingshot = new SlingShot(polygon.body,{x:200, y:300});
 
 }
 
@@ -43,32 +68,68 @@ function draw(){
     background(200);
     Engine.update(engine);
     strokeWeight(4);
-    
-    polygon.display();
 
-    //slingshot.display();
+    text(mouseX + ',' + mouseY, 10, 15);
+  textSize(20);
+  fill("lightyellow");
+  text("Drag the polygon to destroy the blocks",300,30);
+  textSize(10);
+  text("Press Space to get a second Chance to Play!!",650 ,350);
 
-    box1.display();
-    box2.display();
-    box3.display();
-    box4.display();
-    box5.display();
-    box6.display();
-    box7.display();
-    box8.display();
-    box9.display();
-    box10.display();
-    box11.display();
-    box12.display();
-    groundObject.display();
+  
+  fill("blue");
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  fill("white");
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  fill("red");
+  block13.display();
+  block14.display();
+  block15.display();
+  fill("blue");
+  block16.display();
+  fill("blue");
+  blocks1.display();
+  blocks2.display();
+  blocks3.display();
+  blocks4.display();
+  blocks5.display();
+  fill("red");
+  blocks6.display();
+  blocks7.display();
+  blocks8.display();
+  fill("white")
+  blocks9.display();
+  fill("gold");
+
+    groundObject1.display();
+    groundObject2.display();
     
+    imageMode(CENTER)
+    image(polygon_img ,polygon.position.x,polygon.position.y,40,40);
+
+    slingShot.display();
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(polygon.body, {x: mouseX , y: mouseY});
+  Matter.Body.setPosition(this.polygon,{x:mouseX,y:mouseY});
 }
 
-
 function mouseReleased(){
-    slingshot.fly();
+  slingShot.fly();
+}
+
+function keyPressed(){
+  if(keyCode === 32){
+      slingShot.attach(this.polygon);
+  }
 }
